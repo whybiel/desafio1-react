c145de2f99e2ec0768e2b8ea8069d3a3cb720db1
Gabriel Henrique Ferreira de Moraes <90928251+whybiel@users.noreply.github.com>
2022-02-03 20:32:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Desafio extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Estou fazendo meu projeto</h1>
+      </div>
+    );
+  }
+}
+
+export default Desafio;
